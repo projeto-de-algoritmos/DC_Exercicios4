903bcaa97897a7ab3bd3053afe8810ad252a4596
Matheus Henrique <matheushenriquep285@gmail.com>
2023-11-19 09:04:51 -0300

    fix: Problema 218.

--- 218/218.py
+++ 218/218.py
@@ -1,5 +1,5 @@
 import heapq
-
+from sortedcontainers import SortedList
 
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
@@ -13,29 +13,33 @@ class Solution:
         for left, rigth, height in buildings:
             mudancaAltura.append((left, height, 0))     
             mudancaAltura.append((rigth, height, 1))
+    
         mudancaAltura.sort()
 
-        maxHeap = [] # Guarda as altura utilizadas
-        heapq.heappush((maxHeap, 0))
+        alturas = [] # Guarda as altura utilizadas
+        alturas.append(0)
 
         # Loop para comparar as alturas
-        for i in range(len(mudancaAltura)):
-            auxAltura = mudancaAltura[i][0]
+        i = 0
+        while i in range(len(mudancaAltura)):
+            xAtual = mudancaAltura[i][0]
 
-            # Caso os edificio tenham mesma coordenadas iniciais
-            while j < len(mudancaAltura) and mudancaAltura[j][0] == auxAltura:
+            # Caso os edificio tenham as mesmas coordenadas x
+            while i < len(mudancaAltura) and mudancaAltura[i][0] == xAtual:
                 x, y, f = mudancaAltura[i]
 
                 if f == 0:
-                    heapq.heappush((maxHeap, y))
+                    alturas.append(y)
+                    alturas.sort()
                 else:
-                    heapq.heappop(maxHeap)
+                    alturas.remove(y)
+                    alturas.sort()
 
-                j += 1
+                i += 1
 
             # Verificando se a altura modificou
-            if not skyline or skyline[-1][1] != mudancaAltura[-1]:
-                skyline.append((auxAltura, mudancaAltura[-1]))
+            if not skyline or skyline[-1][1] != alturas[-1]:
+                skyline.append((xAtual, alturas[-1]))
 
         return skyline
 
